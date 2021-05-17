66133ae93e232e18a547ad8cddd6d2943207c9be
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-05-17 19:00:49 +0300

    add method for check Time

--- testforma/src/main/java/org/vntu/postavka/testforma/Application.java
+++ testforma/src/main/java/org/vntu/postavka/testforma/Application.java
@@ -2,6 +2,7 @@ package org.vntu.postavka.testforma;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.*;
 
 //import java.text.ParseException;
 //import java.text.SimpleDateFormat;
@@ -10,7 +11,29 @@ import static javax.swing.JOptionPane.*;
 
 public class Application {
 
-//private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d-M-yyyy");
+public static boolean isValidTime(String time) {
+// Regex to check valid time in 24-hour format.
+// Time: 13:05
+  String regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+ 
+// Compile the ReGex
+  Pattern p = Pattern.compile(regex);
+ 
+// If the time is empty
+// return false
+  if (time == null) {
+  return false;
+  }
+ 
+// Pattern class contains matcher() method
+// to find matching between given time
+// and regular expression.
+  Matcher m = p.matcher(time);
+ 
+// Return if the time
+// matched the ReGex
+  return m.matches();
+}
 
 public static boolean isValidDate(final String date) {
 // Returns true if date is in format
@@ -40,7 +63,10 @@ public static void main(String[] args) {
     analitics.setOrderDate(results);
 
   results = showInputDialog("Введіть час замовлення:");
-  analitics.setOrderTime(results);
+  if (!isValidTime(results))
+    showMessageDialog(null, "Помилка у визначенні часу!");
+  else
+    analitics.setOrderTime(results);
 
   results = showInputDialog("Введіть дату реалізації товару:");
   analitics.setCustomerDate(results);
